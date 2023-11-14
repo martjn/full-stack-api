4f5cf53e86a4830ebc6c3c3b3bdd410d488ebdfd
martjn <mingdev77@gmail.com>
2023-11-14 16:21:12 +0200

    fix bug

--- routes/Comments.js
+++ routes/Comments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Comments } = require("../models");
+const { Comments, Logs } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 /**
--- routes/Likes.js
+++ routes/Likes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Likes } = require("../models");
+const { Likes, Logs } = require("../models");
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
 /**
