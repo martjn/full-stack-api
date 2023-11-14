ba764eb6c4f67a7af18fc4d1c09ad00a38c2805f
martjn <mingdev77@gmail.com>
2023-11-14 14:22:37 +0200

    update logs model

--- models/Users.js
+++ models/Users.js
@@ -1,3 +1,5 @@
+const SequelizeAudit = require("sequelize-audit");
+
 module.exports = (sequelize, DataTypes, models) => {
   const Users = sequelize.define("Users", {
     username: {
@@ -9,42 +11,16 @@ module.exports = (sequelize, DataTypes, models) => {
       allowNull: false,
     },
   });
+
+  SequelizeAudit(Users, {
+    strategy: "all",
+  });
+
   Users.associate = (models) => {
     Users.hasMany(models.Likes, {
       onDelete: "cascade",
     });
-    Users.hasMany(models.Logs, {
-      onDelete: "cascade",
-    })
   };
-
-  // Define a hook to log changes to the Results table
-  Users.addHook("afterCreate", async (user, options) => {
-    // Log the creation action to the Results table
-    await models.Logs.create({
-      actionType: "insert",
-      modelName: "Users",
-      invokerId: user.id,
-    });
-  });
-  // Define a hook to log changes to the Results table
-  Users.addHook("afterUpdate", async (user, options) => {
-    // Log the creation action to the Results table
-    await models.Logs.create({
-      actionType: "update",
-      modelName: "Users",
-      invokerId: user.id,
-    });
-  });
-  // Define a hook to log changes to the Results table
-  Users.addHook("afterDestroy", async (user, options) => {
-    // Log the creation action to the Results table
-    await models.Logs.create({
-      actionType: "delete",
-      modelName: "Users",
-      invokerId: user.id,
-    });
-  });
 
   return Users;
 };
