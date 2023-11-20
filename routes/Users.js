c9d3474c61fc463ba3de2879fad4a3e42cdca030
martjn <mingdev77@gmail.com>
2023-11-20 17:24:09 +0200

    Allow user profile api mutation

--- routes/Users.js
+++ routes/Users.js
@@ -151,6 +151,7 @@ router.get("/auth", validateToken, (req, res) => {
  */
 
 router.get("/user/:id", async (req, res) => {
+  let order = [["createdAt", "DESC"]];
   const id = req.params.id;
 
   const basicInfo = await Users.findByPk(id);
@@ -159,11 +160,18 @@ router.get("/user/:id", async (req, res) => {
       username: basicInfo.username,
     },
     include: [Likes],
+    order
+  });
+  const likedPosts = await Likes.findAll({
+    where: {
+      UserId: req.user.id,
+    },
   });
   res.json({
     id: basicInfo.id,
     username: basicInfo.username,
     associatedPosts: associatedPosts,
+    likedPosts: likedPosts,
   });
 });
 
